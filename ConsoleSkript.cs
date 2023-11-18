d639eb5dd805f070534a7e4077a698746b7b6485
Damir137 <151370560+Damir137@users.noreply.github.com>
2023-11-18 14:24:46 +0300

    Add files via upload

---
+++ ConsoleSkript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsoleSkript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(2023);
+        Debug.Log("Hello world");
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
